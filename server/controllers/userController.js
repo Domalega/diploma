8434d760eb2b63536d95bb7854b4972b63432d00
Domalega <vladimir.domalega@gmail.com>
2023-01-19 20:19:30 +0700

    Add jwt and bcrtpy. Create registration for users

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,10 +1,30 @@
-const {Type} = require('../models/models');
+const {UserDate, User} = require('../models/models');
 const ApiError = require('../errors/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken');
+const { json } = require('sequelize');
+
 
 
 class CUserContoller {
-    async regestration(req, res){
+    async regestration(req, res, next){
+        const {email, password, fullName} = req.body
+        if(!email || !password || !fullName)
+            return next(ApiError.badRequest('Uncorrect data'))
 
+        const usedEmail = await User.findOne({where: {email}})
+        if(usedEmail)
+            return next(ApiError.badRequest('User is already have with this email'))
+
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({email, password: hashPassword, fullName})
+        const token = jwt.sign(
+            {id: user.id, email: user.email, fullName: user.fullName}, 
+            process.env.SECRET_KEY,
+            {expiresIn: '24h'}
+        )
+
+        return res.json(token)
     }
 
     async login(req, res){
@@ -12,10 +32,6 @@ class CUserContoller {
     }
 
     async check(req, res, next){
-        const {id} = req.query
-        if(!id)
-            return next(ApiError.badRequest('No id!'))
-        res.json(id)
     }
 }
 
