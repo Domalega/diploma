55e98a47f1ca70f233383824e24d2c244918d631
Domalega <vladimir.domalega@gmail.com>
2023-02-17 10:56:59 +0700

    some router changes in client

---
+++ client/src/pages/Auth.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from 'react-bootstrap/esm/Container';
+
+const Auth = () =>{
+  return(
+    <Container 
+      className=''
+      style={{height: window.height - 54}}
+    > 
+      AUTH
+    </Container>
+  )
+}
+
+export default Auth;
---
+++ client/src/pages/Calendar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Calendar = () =>{
+  return(
+    <div>
+      Calendars
+    </div>
+  )
+}
+
+export default Calendar;
